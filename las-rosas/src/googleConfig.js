1e4307022459f7a578f58c074a964ef95f628525
xmiguel21 <xmiguel32@gmail.com>
2026-06-05 23:14:48 -0400

    fix: actualizar ID planilla Google Sheets

--- las-rosas/src/googleConfig.js
+++ las-rosas/src/googleConfig.js
@@ -1,13 +1,12 @@
 export const GOOGLE_CONFIG = {
   clientId: '707843863477-grleot1n6rrhkuc8c2p5195c4mnhig0o.apps.googleusercontent.com',
-  sheetId: '11ELkq1iaIgvta9nwcXW7HqZYLK1jK0cfErXa8sGe8OM',
+  sheetId: '1RaFiUcNdU6SPCU8QuI3nsyI8x4NJSQNFodUPdiqUAnw',
   scopes: [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive.file',
   ].join(' '),
 }
 
-// Nombres exactos de las hojas
 export const SHEETS = {
   damages:   'Daños Activos',
   resolved:  'Arreglos Realizados',
